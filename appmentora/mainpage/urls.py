7b6fa73a71e382fbcb9b63aaf92f031ddbab3ba2
fafafoyi <fatihefeunal236@gmail.com>
2025-04-01 00:06:54 +0200

    Chat Page Added

--- appmentora/mainpage/urls.py
+++ appmentora/mainpage/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns= [ 
-    path('',views.mainpage,name='mainpage')
+    path('',views.mainpage,name='mainpage'),
+    path('learnmore/', views.learnPage, name='learnPage'),
+    path('chatButton/', views.chatButton, name='chatButton')
 ]
--- appmentora/mainpage/views.py
+++ appmentora/mainpage/views.py
@@ -4,5 +4,12 @@ from django.template import loader
 
 
 def mainpage(request):
-    template = loader.get_template('base.html')
+    template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def learnPage(request):
+    return render(request, 'learn.html')
+
+def chatButton(request):
+    # Add logic for the chat button action
+    return render(request, 'chat.html')
