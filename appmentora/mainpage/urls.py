f4e0715a7d93a0300a03bc64f109827d1b1cd8a8
saricic <saricicekserkann@gmail.com>
2025-04-01 23:26:17 +0200

    backend created

--- appmentora/mainpage/urls.py
+++ appmentora/mainpage/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from .views import clickButton
 
 urlpatterns= [ 
-    path('',views.mainpage,name='mainpage')
+    path('',views.mainpage,name='mainpage'),
+    path('learnmore/',views.learnmorepage,name='learnmorepage'),
+    #path('chat/',views.chatpage,name='chatpage'),
+    path('chat/', views.chatbot, name='chatbot'),
+    path('chat/',clickButton,name='chatButton'),
+    
 ]
--- appmentora/mainpage/views.py
+++ appmentora/mainpage/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from django.urls import reverse
+
+
+
+
 
 
 def mainpage(request):
-    template = loader.get_template('base.html')
+    template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+
+def learnmorepage(request):
+    template = loader.get_template('learn.html')
+    return HttpResponse(template.render())
+
+#def chatpage(request):
+   # template = loader.get_template('chatbot.html')
+   # return HttpResponse(template.render())
+
+
+def clickButton(request):   
+    if request.method == "POST":
+        return redirect('chatpage')
+    return render(request, 'chatbot.html')
+    
+# message works properly but the problem is about issuing the response to the
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from django.http import JsonResponse
+from django.shortcuts import render
+
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'hi hi hi'
+        print(message)
+        #return JsonResponse({'message': message, 'response': response})
+ # please ensure this is executed for get requests.
+    return render(request, 'chatbot.html', {'response': 'hi hi hi'})
+
